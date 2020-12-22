e322ecda3e62db092ef8bfbc8c3a32a283710dbf
unknown <1136031918@qq.com>
2020-12-22 13:57:45 +0800

    add import excel style

--- README.md
+++ README.md
@@ -1,21 +1,60 @@
 # xlsx exchange
-xlsx data format conversion for xspreadsheet
+xlsx data format conversion for x-spreadsheet
 
-### Install
+## 项目介绍
+`xlsx-exchange` 基于xlsx和xlsx-style实现excel文件转换为x-spreadsheet可加载的数据格式
+
+
+## 功能介绍
+* 支持文本内容
+* 支持单元格边框线型、颜色
+* 支持单元格文本对齐方式
+* 支持单元格填充颜色
+* 支持单元格字体大小、颜色
+
+### 引用
 
 ```
+npm install xlsx
+npm install xlsx-style
 npm install xlsx-exchange
 ```
 
-### Usage
+### 使用
 
 **js**
 
 ```javascript
+import XLSX from 'xlsx';
+import XStyle from 'xlsx-style';
 import Exchange from 'xlsx-exchange';
 //导入数据
-let out = Exchange.stox(workbook);
+importFile(inputfile) {
+    let file = inputfile.file;
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var data = e.target.result;
+        var workbook = XStyle.read(data, { type: "binary", cellStyles: true });
+        let out = Exchange.stox(workbook);
+    };
+    reader.readAsBinaryString(file);
+},
 //导出数据
-var new_wb = Exchange.xtos(this.sheet.getData());
+exportFile(outData){
+    var new_wb = Exchange.xtos(outData);
+    XLSX.writeFile(new_wb, "SheetJS.xlsx");
+}
+
 ```
+
+## 其他
+需要注意XLSX和XLSX-style的区别
+使用XLSX社区版时XLSX.read读取不到单元格的字体、边框、对齐方式等
+使用XLSX-style读取不到行高信息，导出数据不能使用writeFile方式（待验证）
+
+thanks 
+https://github.com/SheetJS/sheetjs
+https://github.com/protobi/js-xlsx
+https://github.com/myliang/x-spreadsheet
+
 
--- src/exchange.js
+++ src/exchange.js
@@ -4,29 +4,82 @@ const Exchange = {
     stox(wb) {
         var out = [];
         wb.SheetNames.forEach(function (name) {
-            var o = { name: name, rows: {}, merges: [] };
+            var o = { name: name, rows: {}, cols: {}, merges: [], styles: [] };
             var ws = wb.Sheets[name];
             var aoa = XLSX.utils.sheet_to_json(ws, { raw: false, header: 1 });
             //获取cell内容
+            let styleIndex = 0;
             aoa.forEach(function (r, i) {
                 var cells = {};
                 r.forEach(function (c, j) {
-                    cells[j] = { text: c };
+                    let cellIndex = XLSX.utils.encode_cell({ c: j, r: i });
+                    if (ws[cellIndex]['s']) {
+                        let cellstyle = ws[cellIndex]['s'];
+                        let oCellstyle = {};
+                        cells[j] = { text: c, style: styleIndex }
+                        //填充
+                        if (cellstyle['fill']) {
+                            let bgargb = cellstyle['fill']['fgColor']['rgb'];
+                            oCellstyle.bgcolor = '#' + bgargb.slice(2);
+                        }
+                        //字体
+                        if (cellstyle['font']) {
+                            oCellstyle.font = {
+                                bold: cellstyle['font']['bold'] || false,
+                                size: cellstyle['font']['sz'] || 11
+                            }
+                            if (cellstyle['font']['color']) {
+                                let fcargb = cellstyle['font']['color']['rgb'];
+                                if (fcargb == '000000') {
+                                    oCellstyle.color = '#FFFFFF';
+                                } else {
+                                    oCellstyle.color = '#' + fcargb.slice(2);
+                                }
+                            }
+                        }
+                        //边框
+                        if (cellstyle['border']) {
+                            oCellstyle.border = {};
+                            for (var key in cellstyle['border']) {
+                                let brargb = cellstyle['border'][key]['color']['rgb'];
+                                let obrargb = brargb ? '#' + brargb : "#000000";
+                                oCellstyle.border[key] = [cellstyle['border'][key]['style'], obrargb];
+                            }
+                        }
+                        //对齐
+                        if (cellstyle['alignment']) {
+                            let align = cellstyle['alignment']['horizontal'];
+                            oCellstyle.align = align ? align : 'center';
+                            let valign = cellstyle['alignment']['vertical'];
+                            oCellstyle.valign = (!valign || valign == 'center') ? 'middle' : valign;
+                        }
+                        o.styles[styleIndex] = oCellstyle;
+                        styleIndex++;
+                    } else {
+                        cells[j] = { text: c }
+                    }
                 });
+
                 o.rows[i] = { cells: cells };
             });
             //获取合并单元格
             if (ws["!merges"]) {
                 let merges = ws["!merges"];
-                merges.forEach((item, i) => {
+                merges.forEach(item => {
                     let rMerge = item.e.r - item.s.r;
                     let cMerge = item.e.c - item.s.c;
+                    let range = XLSX.utils.encode_range(item);
                     o.rows[item.s.r]["cells"][item.s.c].merge = [rMerge, cMerge];
-
-                    let scell = XLSX.utils.encode_cell(item.s);
-                    let ecell = XLSX.utils.encode_cell(item.e);
-                    o.merges.push(scell + ":" + ecell);
+                    o.merges.push(range);
                 });
+            }
+            //获取列宽度
+            if (ws["!cols"]) {
+                let cols = ws["!cols"];
+                cols.forEach((item, index) => {
+                    o.cols[index] = {};
+                    o.cols[index]["width"] = item.wpx;
+                })
             }
             out.push(o);
         });
